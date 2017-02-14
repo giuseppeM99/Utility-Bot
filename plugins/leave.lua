93bda644e095199297131bb26c15adf34af8b1c0
giuseppe marino <giuseppe.marino1999@gmail.com>
2017-02-14 19:26:49 +0100

    Improved logging on Leave

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -3,21 +3,37 @@ local function run(msg, matches)
     if matches[1] == msg.text then
       if is_chat_msg(msg) then
         if not is_chan_msg(msg) then
-          chat_del_user(get_receiver(msg), "user#id"..our_id, ok_cb, nil)
-          snoop_msg("Leaving chat " .. msg.to.id)
+          chat_info(get_receiver(msg), get_chat_info, {receiver = _config.LOG_ID})
+          if chat_del_user(get_receiver(msg), "user#id"..our_id, ok_cb, nil) then
+            snoop_msg("Leaving -" .. msg.to.id)
+          else
+            snoop_msg("There was an error in leaving -" .. msg.to.id)
+          end
         else
-          leave_channel(get_receiver(msg), ok_cb, nil)
-          snoop_msg("Leaving channel " .. msg.to.id)
+          if leave_channel(get_receiver(msg), ok_cb, nil) then
+            snoop_msg("Leaving -100" .. msg.to.id)
+          else
+            snoop_msg("There was an error in leaving -100" .. msg.to.id)
+          end
         end
       end
     end
     if matches[1]:match("^-100%d+$") then
-      leave_channel("channel#id"..matches[1]:gsub("-100", ""), ok_cb, nil)
-      snoop_msg("Leaving channel "..matches[1])
+      if leave_channel(matches[1]:gsub("-100", "channel#id"), ok_cb, nil) then
+        snoop_msg("Leaving "..matches[1])
+      else
+        snoop_msg("There was an error in leaving " .. msg.to.id)
+      end
+      return
     end
     if matches[1]:match("^-%d+$") then
-      chat_del_user("chat#id"..matches[1]:gsub("-", ""), "user#id"..our_id, ok_cb, nil)
-      snoop_msg("Leaving chat "..matches[1])
+      chat_info(matches[1]:gsub("-","chat#id"), get_chat_info, {receiver = _config.LOG_ID})
+      if chat_del_user(matches[1]:gsub("-", "chat#id"), "user#id"..our_id, ok_cb, nil) then
+        snoop_msg("Leaving "..matches[1])
+      else
+        snoop_msg("There was an error in leaving " .. msg.to.id)
+      end
+      return
     end
   end
 end
