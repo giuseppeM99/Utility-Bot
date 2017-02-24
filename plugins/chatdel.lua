1a983980f8f2bf96e01f7644f521de32d800a903
giuseppe marino <giuseppe.marino1999@gmail.com>
2017-02-24 16:37:57 +0100

    BugFix What was that?

--- plugins/chatdel.lua
+++ plugins/chatdel.lua
@@ -31,7 +31,7 @@ local function run(msg, matches)
         if msg.from.id == our_id then
           snoop_msg("I've joined the channel " ..msg.to.title .. " [-100" .. msg.to.id .."] via channel username")
         else
-          snoop_msg("I've been added to channel " ..msg.to.title .." [-100" .. msg.to.id .. "] by the user " .. user_print_name(msg.from) .. "[" .. msg.from.id .. "]")
+          snoop_msg("I've been added to channel " ..msg.to.title .." [-100" .. msg.to.id .. "] by the user " .. user_print_name(msg.from) .. " [" .. msg.from.id .. "]")
         end
         channel_info("channel#id" .. msg.to.id, get_channel_info, {receiver = _config.LOG_ID})
       end
@@ -51,26 +51,24 @@ local function run(msg, matches)
       end
     elseif msg.to.type == "channel" and msg.from.id == our_id then
       snoop_msg("I've joined the channel " ..msg.to.title .. " [-100" .. msg.to.id .."] via invite link")
-      if not channel_info("channel#id" .. msg.to.id, get_channel_info, {receiver = _config.LOG_ID}) then
-        snoop_msg("Error can't leave -100" .. msg.to.id)
-      end
+      channel_info("channel#id" .. msg.to.id, get_channel_info, {receiver = _config.LOG_ID})
     end
   end
   if matches[1] == "enable" then
     if not is_enable(matches[2]) then
       table.insert(_config.enable_chats, matches[2])
-      send_large_msg(get_receiver(msg), "chat " .. matches[2] .. " is now enable")
+      send_large_msg(get_receiver(msg), "The chat " .. matches[2] .. " is now enable")
     end
-    send_large_msg(get_receiver(msg), "the chat " .. matches[2] .. " was alredy enable")
+    send_large_msg(get_receiver(msg), "The chat " .. matches[2] .. " was alredy enable")
   end
   if matches[1] == "disable" then
     for k, v in pairs(_config.enable_chats) do
       if v == matches[2] then
         table.remove(_config.enable_chats, k)
-        send_large_msg(get_receiver(msg), "the chat " .. matches[2] .. " is now disable")
+        send_large_msg(get_receiver(msg), "The chat " .. matches[2] .. " is now disable")
         return
       end
-      send_large_msg(get_receiver(msg), "the chat " .. matches[2] .. " wasn't enable")
+      send_large_msg(get_receiver(msg), "The chat " .. matches[2] .. " wasn't enable")
     end
   end
 end
